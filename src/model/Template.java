152e417b882e037edc4e8f4e0d66ae6247e3a6a9
André Fosvold <andrfo@stud.ntnu.no>
2016-03-16 11:46:17 +0100

    Made getters and setters, constructor and fields

--- src/model/Template.java
+++ src/model/Template.java
@@ -13,6 +13,100 @@ public class Template {
 	private String lateTips;
 	private Integer spectators;
 	private Integer airCondition;
+	private Integer weatherTemp;
+	private String weatherType;
+	private boolean isOutdoor;
+	public Template(Integer iD, int pERSON_ID, Integer duration, Date dateTime, Integer healthCondition,
+			Integer performance, String purposeNote, String lateTips, Integer spectators, Integer airCondition,
+			Integer weatherTemp, String weatherType, boolean isOutdoor) {
+		super();
+		ID = iD;
+		PERSON_ID = pERSON_ID;
+		this.duration = duration;
+		this.dateTime = dateTime;
+		this.healthCondition = healthCondition;
+		this.performance = performance;
+		this.purposeNote = purposeNote;
+		this.lateTips = lateTips;
+		this.spectators = spectators;
+		this.airCondition = airCondition;
+		this.weatherTemp = weatherTemp;
+		this.weatherType = weatherType;
+		this.isOutdoor = isOutdoor;
+	}
+	public Integer getDuration() {
+		return duration;
+	}
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+	public Date getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(Date dateTime) {
+		this.dateTime = dateTime;
+	}
+	public Integer getHealthCondition() {
+		return healthCondition;
+	}
+	public void setHealthCondition(Integer healthCondition) {
+		this.healthCondition = healthCondition;
+	}
+	public Integer getPerformance() {
+		return performance;
+	}
+	public void setPerformance(Integer performance) {
+		this.performance = performance;
+	}
+	public String getPurposeNote() {
+		return purposeNote;
+	}
+	public void setPurposeNote(String purposeNote) {
+		this.purposeNote = purposeNote;
+	}
+	public String getLateTips() {
+		return lateTips;
+	}
+	public void setLateTips(String lateTips) {
+		this.lateTips = lateTips;
+	}
+	public Integer getSpectators() {
+		return spectators;
+	}
+	public void setSpectators(Integer spectators) {
+		this.spectators = spectators;
+	}
+	public Integer getAirCondition() {
+		return airCondition;
+	}
+	public void setAirCondition(Integer airCondition) {
+		this.airCondition = airCondition;
+	}
+	public Integer getWeatherTemp() {
+		return weatherTemp;
+	}
+	public void setWeatherTemp(Integer weatherTemp) {
+		this.weatherTemp = weatherTemp;
+	}
+	public String getWeatherType() {
+		return weatherType;
+	}
+	public void setWeatherType(String weatherType) {
+		this.weatherType = weatherType;
+	}
+	public boolean isOutdoor() {
+		return isOutdoor;
+	}
+	public void setOutdoor(boolean isOutdoor) {
+		this.isOutdoor = isOutdoor;
+	}
+	public Integer getID() {
+		return ID;
+	}
+	public int getPERSON_ID() {
+		return PERSON_ID;
+	}
+	
 }
 /*
 CREATE TABLE TRAINING_SESSION(
